6a1e81e2cc422ae2e53e9404eb3217d2d1d1040d
Mae Koger <mkoger24@wooster.edu>
2024-02-22 21:07:09 -0500

    posting comments

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, send_file, redirect, request#, abortr
 import mysql.connector
+import datetime
 
 mydb = mysql.connector.connect( 
     host = "localhost", 
@@ -42,7 +43,7 @@ def searchTerm():
         
         # if there are no specified search parameters, display all entries
         else:
-            query = "SELECT ElectronicISBN, BookTitle, Author FROM books;"
+            query = "SELECT id, ElectronicISBN, BookTitle, Author FROM books;"
             mycursor = mydb.cursor() 
             mycursor.execute(query) 
             dbhtml = mycursor.fetchall() 
@@ -55,18 +56,18 @@ def searchTerm():
 
         # specifying a query to use based on the intended column to search
         elif column == 'BookTitle':
-            query = "SELECT ElectronicISBN, BookTitle, Author FROM books WHERE BookTitle like '%" + searchTerm + "%';"
+            query = "SELECT id, ElectronicISBN, BookTitle, Author FROM books WHERE BookTitle like '%" + searchTerm + "%';"
 
         elif column == 'ElectronicISBN':
-            query = "SELECT ElectronicISBN, BookTitle, Author FROM books WHERE ElectronicISBN like '%" + searchTerm + "%';"
+            query = "SELECT id, ElectronicISBN, BookTitle, Author FROM books WHERE ElectronicISBN like '%" + searchTerm + "%';"
 
         elif column == 'Author':
-            query = "SELECT ElectronicISBN, BookTitle, Author FROM books WHERE Author like '%" + searchTerm + "%';"
+            query = "SELECT id, ElectronicISBN, BookTitle, Author FROM books WHERE Author like '%" + searchTerm + "%';"
 
         # if all columns is specified
         # TODO test this functionality
         elif column == 'All':
-            query = "SELECT ElectronicISBN, BookTitle, Author FROM books WHERE BookTitle like'%" + searchTerm + "%' or WHERE ElectronicISBN like'%" + searchTerm + "%' or WHERE Author like '%" + searchTerm + "%';"
+            query = "SELECT id, ElectronicISBN, BookTitle, Author FROM books WHERE BookTitle like'%" + searchTerm + "%' or WHERE ElectronicISBN like'%" + searchTerm + "%' or WHERE Author like '%" + searchTerm + "%';"
 
         # run the query and return the template
         mycursor = mydb.cursor() 
@@ -78,21 +79,57 @@ def searchTerm():
     except Exception as e: 
         return(str(e))
 
-@app.route('/details.html', methods=['GET'])
+@app.route('/details.html', methods=['GET','POST'])
 def display():
 
     try:
-        isbn = request.args.get("q")
-        bookInfoQuery = "SELECT * FROM books WHERE ElectronicISBN = '" + isbn + "';"
-        mycursor = mydb.cursor() 
-        mycursor.execute(bookInfoQuery) 
-        bookdb = mycursor.fetchall() 
+        if request.method == "POST":
+            id = request.args.get("q")
+            comment = request.form["userComment"]
 
-        commentQuery = "SELECT * FROM comments WHERE ISBN = '" + isbn + "';"
-        mycursor = mydb.cursor() 
-        mycursor.execute(commentQuery) 
-        commentdb = mycursor.fetchall() 
-        return render_template("details.html", bookdb = bookdb, commentdb = commentdb, isbn = isbn)
+            now = datetime.datetime.now()
+
+            month = str(now.month) 
+            day = str(now.day) 
+            year = str(now.year)
+            hour = str(now.hour)
+            minute = now.minute
+            if minute < 10:
+                minute = "0"+str(minute)
+            else:
+                minute = str(minute)
+            dateTime = month +"-"+ day +"-"+ year +" "+ hour +":"+ minute
+
+
+            query = "INSERT INTO comments (book_id, Comment, DateTime, User) VALUES ('" + id + "', '" + comment +"', '" + dateTime + "', 'testuser');"
+            mycursor = mydb.cursor() 
+            mycursor.execute(query) 
+            bookdb = mycursor.fetchall() 
+
+            id = request.args.get("q")
+            bookInfoQuery = "SELECT * FROM books WHERE id = '" + id + "';"
+            mycursor = mydb.cursor() 
+            mycursor.execute(bookInfoQuery) 
+            bookdb = mycursor.fetchall() 
+
+            commentQuery = "SELECT * FROM comments WHERE book_id = '" + id + "';"
+            mycursor = mydb.cursor() 
+            mycursor.execute(commentQuery) 
+            commentdb = mycursor.fetchall() 
+            return render_template("details.html", bookdb = bookdb, commentdb = commentdb, id = id)
+        else:
+
+            id = request.args.get("q")
+            bookInfoQuery = "SELECT * FROM books WHERE id = '" + id + "';"
+            mycursor = mydb.cursor() 
+            mycursor.execute(bookInfoQuery) 
+            bookdb = mycursor.fetchall() 
+
+            commentQuery = "SELECT * FROM comments WHERE book_id = '" + id + "';"
+            mycursor = mydb.cursor() 
+            mycursor.execute(commentQuery) 
+            commentdb = mycursor.fetchall() 
+            return render_template("details.html", bookdb = bookdb, commentdb = commentdb, id = id)
     
     except Exception as e: 
         return(str(e))
